f3f1ba562c517c13f7bfed19c5be1a7781b7b55c
philip omoigui <philipomoigui@gmail.com>
2020-10-06 00:39:42 +0100

    Minor fixes

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clara.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
