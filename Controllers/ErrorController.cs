aa84a1709c1ae53f41274390ce49031547d3f55d
philip omoigui <philipomoigui@gmail.com>
2020-10-17 01:13:04 +0100

    Added Error Pages

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clara.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/Error/{statusCode}")]
+        public IActionResult StatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Oops, that page is not here.";
+                    break;
+            }
+            return View("NotFound");
+        }
+
+        [Route("/Error")]
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -62,11 +62,13 @@ namespace Clara
         {
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
